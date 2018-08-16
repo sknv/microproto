61e448b2673c600e919710268e566ad92c2d8ac9
skanukov <s.kanukov@gmail.com>
2018-08-16 13:34:58 +0300

    use alias for rpc package

--- app/math/server/mathserver.go
+++ app/math/server/mathserver.go
@@ -2,34 +2,34 @@ package server
 
 import (
 	"context"
-	"math"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/sknv/microproto/app/math/rpc"
+	math "github.com/sknv/microproto/app/math/rpc"
 )
 
 type MathServer struct{}
 
-func (*MathServer) Rect(_ context.Context, args *rpc.RectArgs) (*rpc.RectReply, error) {
-	if args.Width <= 0 || args.Height <= 0 {
-		return nil, status.Error(codes.InvalidArgument, "width and height must be positive numbers")
-	}
-
-	return &rpc.RectReply{
-		Perimeter: 2*args.Width + 2*args.Height,
-		Square:    args.Width * args.Height,
-	}, nil
-}
-
-func (*MathServer) Circle(_ context.Context, args *rpc.CircleArgs) (*rpc.CircleReply, error) {
+func (*MathServer) Circle(_ context.Context, args *math.CircleArgs) (*math.CircleReply, error) {
 	if args.Radius <= 0 {
 		return nil, status.Error(codes.InvalidArgument, "radius must be a positive number")
 	}
 
-	return &rpc.CircleReply{
-		Length: 2 * math.Pi * args.Radius,
-		Square: math.Pi * args.Radius * args.Radius,
+	pi := 3.1416 // there is math.Pi constant in the standard lib btw
+	return &math.CircleReply{
+		Length: 2 * pi * args.Radius,
+		Square: pi * args.Radius * args.Radius,
+	}, nil
+}
+
+func (*MathServer) Rect(_ context.Context, args *math.RectArgs) (*math.RectReply, error) {
+	if args.Width <= 0 || args.Height <= 0 {
+		return nil, status.Error(codes.InvalidArgument, "width and height must be positive numbers")
+	}
+
+	return &math.RectReply{
+		Perimeter: 2*args.Width + 2*args.Height,
+		Square:    args.Width * args.Height,
 	}, nil
 }
